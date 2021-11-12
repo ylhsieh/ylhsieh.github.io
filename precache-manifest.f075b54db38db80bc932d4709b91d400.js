d725a9715f2f33621fd7705498e78c270a9aeaf6
morphe <morphe@iis.sinica.edu.tw>
2021-11-12 15:18:51 +0800

    Initial

---
+++ precache-manifest.f075b54db38db80bc932d4709b91d400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8944fa1a49cd7abac1bda2428de3c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e981d420e8b7fc5bb4b8",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "9d655856d014b0c4137b",
+    "url": "/static/css/main.e5162f64.chunk.css"
+  },
+  {
+    "revision": "e981d420e8b7fc5bb4b8",
+    "url": "/static/js/2.d7bf6fb7.chunk.js"
+  },
+  {
+    "revision": "9d655856d014b0c4137b",
+    "url": "/static/js/main.e3d10f7b.chunk.js"
+  },
+  {
+    "revision": "c06b7b300d045328f31d",
+    "url": "/static/js/runtime~main.60d0dddc.js"
+  }
+]);
